eecb9a89184b649b1881b1248168ea5a532ee44d
Theanhmgt <theanhmgt66@gmail.com>
2024-04-09 12:27:44 +0700

    feat(view,repository): implement fetch and show booking detail in payment nav

--- SingleHotelManagement/app/main.py
+++ SingleHotelManagement/app/main.py
@@ -1,6 +1,7 @@
 from app import init_tables, app
-
+from app.services.booking_service import get_info_booking
 if __name__ == '__main__':
     # init_tables()
     with app.app_context():
+        print(get_info_booking(4))
         app.run(debug=True)
